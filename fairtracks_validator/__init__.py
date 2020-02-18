c06c7708ebd93d4ff31d8a197e5e2bbc80e76160
José María Fernández <jose.m.fernandez@bsc.es>
2020-02-18 12:09:31 +0100

    Now Owlready2 has officially released 0.23, update the dependency and version bump fairtracks_validator

--- fairtracks_validator/__init__.py
+++ fairtracks_validator/__init__.py
@@ -1,1 +1,4 @@
+# -*- coding: utf-8 -*-
+
 name = "fairtracks_validator"
+__version__ = version = '0.9.4'
--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@
 
 import setuptools
 import re
+import os
+import sys
+
+# In this way, we are sure we are getting
+# the installer's version of the library
+# not the system's one
+sys.path.insert(0,os.path.dirname(__file__))
+
+from fairtracks_validator import version as fairtracks_validator_version
 
 # Populating the long description
 with open("README.md", "r") as fh:
@@ -19,7 +28,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
 	name="fairtracks_validator",
-	version="0.9.3",
+	version=fairtracks_validator_version,
 	scripts=["fairGTrackJsonValidate.py"],
 	author="José Mª Fernández",
 	author_email="jose.m.fernandez@bsc.es",
@@ -30,7 +39,6 @@ setuptools.setup(
 	packages=setuptools.find_packages(),
 	install_requires=requirements,
 	classifiers=[
-		"Programming Language :: Python :: 2",
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
 		"Operating System :: OS Independent",
